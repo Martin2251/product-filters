c0fd335b5f8bf4c99c37625caf69e4ea5ee01c0c
Martin Lowe <martinlowe673@gmail.com>
2024-08-05 19:21:03 +0100

    product validator

--- src/lib/validators/product-validator.ts
+++ src/lib/validators/product-validator.ts
@@ -1,8 +1,13 @@
 import {z} from "zod"
 
 export const AVAILABLE_SIZES=["S", "M", "L"] as const
+export const AVAILABLE_COLORS=["white","beige", "green", "purple", "blue"] as const
+
+export const AVAILABLE_SORT =["none", "price-asc", "price-desc"] as const
 
 export const ProductFilterValidator = z.object({
-    size:z.array(z.enum(AVAILABLE_SIZES))
+    size:z.array(z.enum(AVAILABLE_SIZES)),
+    color:z.array(z.enum(AVAILABLE_COLORS)),
+    sort: z.enum()
 
 })

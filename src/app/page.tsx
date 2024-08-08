a56b05feab54bce9dd9450fa91b6d6257b18d8a5
Martin Lowe <martinlowe673@gmail.com>
2024-08-08 07:49:57 +0100

    apply array filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -166,7 +166,12 @@ export default function Home() {
                 <ul className="space-y-4">
                   {COLOR_FILTERS.options.map((option, optionIdx)=>(
                     <li key={option.value} className="flex items-center">
-                      <input type="checkbox" id={`color${optionIdx}`} onChange={()=>{}}className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"  />
+                      <input type="checkbox" id={`color${optionIdx}`} onChange={()=>{
+                        applyArrayFilter({
+                          category:"color",
+                          value:option.value
+                        })
+                      }}className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"  />
                       <label htmlFor={`color${optionIdx}`} className="ml-3 text-sm text-gray-600">
                         {option.label}
                       </label>

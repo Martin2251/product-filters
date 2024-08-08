e29c2084a1ecc197a0dbd829cdcb8960a24cb06e
Martin Lowe <martinlowe673@gmail.com>
2024-08-08 07:51:28 +0100

    checked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -171,7 +171,9 @@ export default function Home() {
                           category:"color",
                           value:option.value
                         })
-                      }}className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"  />
+                      }}
+                      checked={filter.color.includes(option.value)}
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"  />
                       <label htmlFor={`color${optionIdx}`} className="ml-3 text-sm text-gray-600">
                         {option.label}
                       </label>

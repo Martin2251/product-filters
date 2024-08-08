d4df3ed1ca4048ec6485f0fbad4b4c268b87163a
Martin Lowe <martinlowe673@gmail.com>
2024-08-08 08:10:59 +0100

    size filters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,6 +35,17 @@ const COLOR_FILTERS ={
   ] as const,
 }
 
+const SIZE_FILTERS ={
+  id:"size",
+  name:"Size",
+  options:[
+    {value:"S", label:"S"},
+    {value:"M", label:"M"},
+    {value:"L", label:"L"},
+   
+  ] as const,
+}
+
 const SUBCATEGORIES = [
 {name: "T-Shirts", selected: true,href:"#"},
 {name: "Hoodies", selected: false,href:"#"},
@@ -52,6 +63,8 @@ export default function Home() {
     size:["L","M","S"],
     sort:"none",
   })
+
+  console.log(filter)
 
   const {data:products} = useQuery({
     queryKey:["products"],
@@ -96,7 +109,7 @@ export default function Home() {
 
   }
 
-  console.log(products)
+
   return (
 <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
   <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
@@ -165,6 +178,33 @@ export default function Home() {
               <AccordionContent className="pt-6 animate-none">
                 <ul className="space-y-4">
                   {COLOR_FILTERS.options.map((option, optionIdx)=>(
+                    <li key={option.value} className="flex items-center">
+                      <input type="checkbox" id={`color${optionIdx}`} onChange={()=>{
+                        applyArrayFilter({
+                          category:"color",
+                          value:option.value
+                        })
+                      }}
+                      checked={filter.color.includes(option.value)}
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"  />
+                      <label htmlFor={`color${optionIdx}`} className="ml-3 text-sm text-gray-600">
+                        {option.label}
+                      </label>
+                    </li>
+                  ))}
+
+                </ul>
+
+              </AccordionContent>
+            </AccordionItem>
+            {/* size filter */}
+            <AccordionItem value="size">
+              <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
+                <span className="font-medium text-gray-900">Color</span>
+              </AccordionTrigger>
+              <AccordionContent className="pt-6 animate-none">
+                <ul className="space-y-4">
+                  {SIZE_FILTERS.options.map((option, optionIdx)=>(
                     <li key={option.value} className="flex items-center">
                       <input type="checkbox" id={`color${optionIdx}`} onChange={()=>{
                         applyArrayFilter({
